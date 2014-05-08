3ccbad063db1c4150c876e8a6cd0d49428c88581
bingo <790354418@qq.com>
2014-05-08 11:36:26 +0800

    Facebook Pop Engine
    Test the cocoa pods and the Facebook pop engine.

---
+++ PopAnimation/PopAnimation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PopAnimation
+//
+//  Created by tian on 14-5-8.
+//  Copyright (c) 2014年 tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PopAnimation/PopAnimation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PopAnimation
+//
+//  Created by tian on 14-5-8.
+//  Copyright (c) 2014年 tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
